636e27decb3e26e66498c9146a08130c662f334d
UnstoppableMango <runner@fv-az658-657.lxhe5nczbbeupcbb3gjj1uzxzg.dx.internal.cloudapp.net>
2024-05-17 05:16:18 +0000

    make build_sdks

--- sdk/python/unmango_pulumi_pihole/dns_record.py
+++ sdk/python/unmango_pulumi_pihole/dns_record.py
@@ -114,7 +114,7 @@ class DnsRecord(pulumi.CustomResource):
         ## Import
 
         ```sh
-         $ pulumi import pihole:index/dnsRecord:DnsRecord record foo.com
+        $ pulumi import pihole:index/dnsRecord:DnsRecord record foo.com
         ```
 
         :param str resource_name: The name of the resource.
@@ -145,7 +145,7 @@ class DnsRecord(pulumi.CustomResource):
         ## Import
 
         ```sh
-         $ pulumi import pihole:index/dnsRecord:DnsRecord record foo.com
+        $ pulumi import pihole:index/dnsRecord:DnsRecord record foo.com
         ```
 
         :param str resource_name: The name of the resource.
